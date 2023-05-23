1efe8a1fb18949a604a7cd77def0912152264d3b
Carlos Alberto Silva Junior <casjunior93@gmail.com>
2023-05-23 08:29:07 -0300

    Ajuste

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def previsao(ar: str, info: str, horas: int):
         index_str = previsoes.index.astype('str')
         previsoes = previsoes.reindex(index_str)
         resposta = {
-            'treinamento': '2023-05-21 15:50:00',
+            'treinamento': '2023-05-23 08:24:00',
             'anteriores': ultimas_horas_com_ar,
             'previsao' : previsoes.to_dict()
             }
@@ -41,7 +41,7 @@ def previsao(ar: str, info: str, horas: int):
         index_str = previsoes.index.astype('str')
         previsoes = previsoes.reindex(index_str)
         resposta = {
-            'treinamento': '2023-05-21 15:50:00',
+            'treinamento': '2023-05-23 08:24:00',
             'anteriores': ultimas_horas_sem_ar,
             'previsao' : previsoes.to_dict()
             }
